70632fa52473ba6cfcdab75ea87bf0c968f4bbd5
Timothy-Manwell <Timothy-Manwell@users.noreply.github.com>
2022-11-15 19:24:55 +0000

    Prettified Code!

--- js/script.js
+++ js/script.js
@@ -18,7 +18,7 @@ if (navigator.serviceWorker) {
 /**
  * This function generates a random number between 1 and 6
  */
-const randomNumber = Math.floor(Math.random() * 6) + 1  // returns a random integer from 1 to 6 into variable "randomNumber" 
+const randomNumber = Math.floor(Math.random() * 6) + 1 // returns a random integer from 1 to 6 into variable "randomNumber"
 
 /**
  * This function updates the slider value.
